226b785cfaa87d466fe34fc4a51ccb2a39d90f37
Romain <rom.prevot@gmail.com>
2014-08-03 01:32:50 +0200

    First commit

---
+++ gyrotest-server/src/main/java/net/collabwork/gyrotest/server/App.java
@@ -0,0 +1,13 @@
+package net.collabwork.gyrotest.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
